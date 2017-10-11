57b6cf25add89dd67110301e353d8e1deacab691
Matt Hough <matt@hough.tv>
2017-10-11 21:29:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @status == "complete" && @sender.valid? == false
+    if @status != "complete" && @sender.valid? == false
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
