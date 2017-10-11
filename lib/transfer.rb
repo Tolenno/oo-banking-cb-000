82de62399e2a917e7dbac0c3ceaf9ae3ade3bad7
Matt Hough <matt@hough.tv>
2017-10-11 21:31:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif @sender.valid?
+    elsif @sender.valid? == false
       "Transaction rejected. Please check your account balance."
     else
       "Double spend protector"
